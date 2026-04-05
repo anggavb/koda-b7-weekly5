d1609dd5537e448aab1e767fa40b5d57c37d3de8
anggavb <anggavb8@gmail.com>
2026-04-05 10:55:37 +0700

    refactor(utils): update listMenus to use prefix for navigation paths

--- src/utils/listMenus.js
+++ src/utils/listMenus.js
@@ -1,39 +1,40 @@
 import { DashboardIcon, TransferIcon, HistoryIcon, TopUpIcon, ProfileIcon, LogoutIcon } from '@components/icons';
 
+const prefix = '/admin'
 export default [
   {
     name: 'Dashboard',
-    to: '/dashboard',
+    to: `${prefix}/dashboard`,
     icon: DashboardIcon,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
     name: 'Transfer',
-    to: '/transfer',
+    to: `${prefix}/transfer`,
     icon: TransferIcon,
     class: 'fill-gray-600 group-aria-[current=page]:fill-current group-hover:fill-blue-800',
   },
   {
     name: 'History',
-    to: '/history',
+    to: `${prefix}/history`,
     icon: HistoryIcon,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
     name: 'Top Up',
-    to: '/topup',
+    to: `${prefix}/topup`,
     icon: TopUpIcon,
     class: 'fill-gray-600 group-aria-[current=page]:fill-current group-hover:fill-blue-800',
   },
   {
     name: 'Profile',
-    to: '/profile',
+    to: `${prefix}/profile`,
     icon: ProfileIcon,
     class: 'stroke-gray-600 group-aria-[current=page]:stroke-current group-hover:stroke-blue-800',
   },
   {
     name: 'Logout',
-    to: '/logout',
+    to: `${prefix}/logout`,
     icon: LogoutIcon,
     class: 'stroke-red-600 group-hover:stroke-red-800',
   },
