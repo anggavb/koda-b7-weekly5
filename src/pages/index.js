693f65962bc8c3986beb41984adc1aeb4d159717
anggavb <anggavb8@gmail.com>
2026-04-05 10:54:26 +0700

    feat(pages): add LandingPage and update index exports

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import History from "./History";
 import Profile from "./Profile";
 import TopUp from "./TopUp";
 import Transfer from "./Transfer";
+import LandingPage from "./LandingPage";
 
 export {
   Dashboard,
@@ -10,4 +11,5 @@ export {
   Profile,
   TopUp,
   Transfer,
+  LandingPage
 }
