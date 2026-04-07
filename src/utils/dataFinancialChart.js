9b821f2e9866f2e2077ae4910f25b4f740f2e7de
anggavb <anggavb8@gmail.com>
2026-04-07 07:32:13 +0700

    feat(chart): add dataFinancialChart for income and expense visualization

---
+++ src/utils/dataFinancialChart.js
@@ -0,0 +1,9 @@
+export default [
+  { day: 'Sat', income: 85000, expense: 20000 },
+  { day: 'Sun', income: 84000, expense: 55000 },
+  { day: 'Mon', income: 90000, expense: 68000 },
+  { day: 'Tue', income: 32000, expense: 35000 },
+  { day: 'Wed', income: 25000, expense: 5000 },
+  { day: 'Thu', income: 75000, expense: 62000 },
+  { day: 'Fri', income: 5000,  expense: 50000 },
+];
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,11 @@
 import listMenus from './listMenus';
 import listWidget from './listWidget';
 import listHistoryTransactions from './listHistoryTransactions';
+import dataFinancialChart from './dataFinancialChart';
 
 export {
   listMenus,
   listWidget,
-  listHistoryTransactions
+  listHistoryTransactions,
+  dataFinancialChart,
 };
