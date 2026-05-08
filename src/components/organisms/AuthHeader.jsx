29aa4b2c17bb33c7c23fcdd1b4caa37b27c5fd5e
anggavb <anggavb8@gmail.com>
2026-05-08 16:33:54 +0700

    chore(AuthHeader): update wallet image source to money-wallet-black.png

--- src/components/organisms/AuthHeader.jsx
+++ src/components/organisms/AuthHeader.jsx
@@ -3,7 +3,7 @@ const AuthHeader = ({ title, subtitle }) => {
     <>
       <header className="mb-2">
         <div className="flex items-center gap-2.5 text-xl font-semibold text-blue-700 font-logo">
-          <img src="/money-wallet.png" alt="wallet" className="h-8" />
+          <img src="/money-wallet-black.png" alt="wallet" className="h-8" />
         </div>
       </header>
 
